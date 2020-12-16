b91d95dc4441557e08aaee837da9852eba03c317
happyjy <okwoyjy@gmail.com>
2020-12-17 07:01:31 +0900

    test getAll, getOne

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -9,15 +10,47 @@ describe('MoviesService', () => {
       providers: [MoviesService],
     }).compile();
 
+    // MoviesService로 접근 가능
     service = module.get<MoviesService>(MoviesService);
   });
 
+  // it: individual test?
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it("sholud be 4", ()=>{
-    expect(2+2).toEqual(4);
-  })
+  it('sholud be 4', () => {
+    expect(2 + 2).toEqual(4);
+    ``;
+  });
 
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      // expect(movie.id).toEqual(1);
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        // expect(e.message).toEqual('Movie with ID 999 not found.');
+      }
+    });
+  });
 });
