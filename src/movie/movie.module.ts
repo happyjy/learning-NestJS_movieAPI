e8c347a84c30496cb1e335f3e744fb64fc49e911
happyjy <okwoyjy@gmail.com>
2020-12-18 06:10:35 +0900

    4.0 Testing movies

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from 'src/movies/movies.controller';
-import { MoviesService } from 'src/movies/movies.service';
+import { MoviesController } from '../movies/movies.controller';
+import { MoviesService } from '../movies/movies.service';
 
 // # app.module.ts에서 이 파일을 import하게 만들어 모듈로 분리 했다.
 // # providers가 contorllers에 있는 모든 것을 immport해서 타입을 추가하는 것만으로 잘 작동하는것.
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -16,9 +16,40 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return request(app.getHttpServer()) //app.getHttpServer(): http://localhost:3000/
       .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .expect(200) // http status code
+      .expect('Welcome to my Movie API'); //Response Code
+  });
+
+  describe('/movies', () => {
+    // /movies get방식으로 resuest했을때 movie
+    it('/GET', () => {
+      return request(app.getHttpServer())
+        .get('/movies') //
+        .expect(200)
+        .expect([]); // 첫 테스트 할때 빈 db이기 때문에 이렇게 해야한다.
+    });
+
+    // localhost:3000/movies를 post로 요청할때
+    // send data를 보내고 201(http status code)를 받는지 확인하는 테스트
+    it('/POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies') //
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+        })
+        .expect(201);
+    });
+
+    // 404: NotFound
+    // 405: MethodNotAllowed
+    it('/POST', () => {
+      return request(app.getHttpServer())
+        .delete('/movies') //
+        .expect(404);
+    });
   });
 });
